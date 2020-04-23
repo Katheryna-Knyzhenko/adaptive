92f2e4e72c8cea1f080ff736bae67fedcbc0fdb3
Kate <evgen.knyzhenko@gmail.com>
2020-04-23 21:52:16 +0300

    designMainPage

---
+++ src/MainPage/Main.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+
+    return (
+        <div>Адаптивчик</div>
+    )
+  }
+}
+export default Main;

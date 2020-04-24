97fabd8496720b43bfac1179baf3c6193143e0e9
Kate <evgen.knyzhenko@gmail.com>
2020-04-24 16:39:50 +0300

    designMainPage

--- src/MainPage/Main.js
+++ src/MainPage/Main.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import styles from './Main.module.css';
 
 class Main extends Component {
   constructor() {
@@ -9,7 +10,19 @@ class Main extends Component {
   render() {
 
     return (
-        <div>Адаптивчик</div>
+        <div className={styles.wrapper}>
+          <div className={styles.content}>Header</div>
+          <header className={styles.header}>
+          <div className={styles.conteiner}>
+            <div className={styles.section}>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+          </div>
+          </header>
+          <div className={styles.content}>Footer</div>
+        </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
-
 import Main from './MainPage/Main';
 
 ReactDOM.render(

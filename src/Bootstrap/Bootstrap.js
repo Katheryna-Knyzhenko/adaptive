44375d774c733f0ec00e08db1ad8091c3fafc907
Kate <evgen.knyzhenko@gmail.com>
2020-05-02 16:01:59 +0300

    BootstrapLessons

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -18,14 +18,14 @@ class Bootstrap extends Component {
             <div className="container-fluid wrapper mt-0">
                 <div className='mt-2 header'>Bootstrap grid!</div>
             <div className="row mt-4">
-            <div className="col-sm bg-info text_center ml-3 ">
+            <div className="col-sm  text_center ml-3 ">
             Первая из трёх колонок
         </div>
-        <div className="col-sm bg-success ml-3 mr-3 p-3 text_center ">
+        <div className="col-sm  ml-3 mr-3 p-3 text_center ">
             Вторая из трёх колонок
         </div>
 
-        <div className="col-sm bg-info p-3 text_center mr-3">
+        <div className="col-sm  p-3 text_center mr-3">
             Третья из трёх колонок
         </div>
             </div>

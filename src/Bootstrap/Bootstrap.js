53ac58d4febf8a60be88d9fb23610b6ddb62fa10
Kate <evgen.knyzhenko@gmail.com>
2020-05-03 16:32:53 +0300

    BootstrapLessons

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -18,19 +18,19 @@ class Bootstrap extends Component {
             <div className="container-fluid wrapper">
                 <div className='row'><div className=' header col-md-12'>Bootstrap grid!</div></div>
             <div className="row container-fluid mt-4">
-            <div className="col-sm-6 col-md-5 col-lg-12 d-flex flex-row align-items-start my-column">
+            <div className="col-sm-6 col-md-5 col-lg-12 d-flex flex-row align-items-start my-column justify-content-start">
                 <div className="p-2 " >column 1</div>
                 <div className="p-2">column 2</div>
                 <div className="p-2">column 3</div>
 
         </div>
-        <div className="col-sm-6 col-md-5 col-lg-12 d-flex flex-row align-items-center my-column">
+        <div className="col-sm-6 col-md-5 col-lg-12 d-flex flex-row align-items-center my-column justify-content-around">
             <div className="p-2">column 1</div>
             <div className="p-2">column 2</div>
             <div className="p-2">column 3</div>
         </div>
 
-        <div className="col-sm-6 col-md-12 col-lg-12 d-flex flex-row align-items-end my-column">
+        <div className="col-sm-6 col-md-12 col-lg-12 d-flex flex-row align-items-end my-column justify-content-end">
             <div className="p-2">column 1</div>
             <div className="p-2">column 2</div>
             <div className="p-2">column 3</div>

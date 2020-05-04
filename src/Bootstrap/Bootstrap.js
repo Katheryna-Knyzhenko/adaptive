20682f85b1b24beb2a5512a127b6c2b5944f5dd2
Kate <evgen.knyzhenko@gmail.com>
2020-05-04 15:53:00 +0300

    BootstrapLessons

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
+
 class Bootstrap extends Component {
     constructor() {
         super();
@@ -19,11 +20,16 @@ class Bootstrap extends Component {
                 <div className='row'><div className=' header col-md-12'>Bootstrap grid!</div></div>
             <div className="row container-fluid mt-4">
             <div className="col-sm-6 col-md-5 col-lg-12 d-flex flex-row align-items-start my-column justify-content-start">
-                <div className="p-2 pr-1 pl-1" > column 1 </div>
-                <div className="p-2"> column 2 </div>
-                <div className="p-2"> column 3 </div>
+                <div className="p-2"><button type="button" className="btn btn-danger btn-lg buttonColumn1row1">
+                    <span className="glyphicon glyphicon-star"></span> Избранное</button>
+            </div>
+                <div className="p-2"><button type="button" className="btn btn-info btn-lg buttonColumn1row1">
+                    <span className="glyphicon glyphicon-leaf"></span> Читать книгу</button></div>
+                <div className="p-2"><button type="button" className="btn btn-secondary btn-lg buttonColumn1row1">
+                    <span className="glyphicon glyphicon glyphicon-folder-open"></span> Выбрать книгу</button></div>
 
         </div>
+
         <div className="col-sm-6 col-md-5 col-lg-12 d-flex flex-row align-items-center my-column justify-content-around">
             <div className="p-2"> column 1 </div>
             <div className="p-2"> column 2 </div>
@@ -36,15 +42,17 @@ class Bootstrap extends Component {
             <div className="p-2"> column 3 </div>
         </div>
             </div>
+
                 <div className="card my-card">
                     <img className="card-img-top top"  src="https://www.scitecheuropa.eu/wp-content/uploads/2019/09/Colour-bird-e1568033112774.jpg" alt="card"></img>
                     <div className="card-body">
-                        <h4 className="card-title">Bird one</h4>
-                        <h6 className="card-subtitle mb-2 text-muted">Птичка 1</h6>
+                        <h4 className="card-title cardTitle">Bird one</h4>
+                        <h6 className="card-subtitle mb-2 text-muted cardSubtitle">Птичка 1 <span className='#869-f'></span></h6>
                         <p className="card-text text">Colourful plumage is believed to have a great function in how birds select a mate, however it could also have an impact on several other physiological processes. New research has suggested that the colourful pigment of a bird might affect its microbial resistance.</p>
                     </div>
 
                 </div>
+                
 
                 <div className='row' ><div className="footer col-md-12">The end</div></div>
     </div>
